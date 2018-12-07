8a094d1371eecc48bf7b28d2b02000e8ca10bbaf
Barnabás Illanicz <barnabas941@gmail.com>
2018-12-07 19:09:40 +0100

    Initial Commit

---
+++ 3DConverter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  3DConverter
+//
+//  Created by Barnabás Illanicz on 2018. 12. 07..
+//  Copyright © 2018. Barnabas Illanicz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
